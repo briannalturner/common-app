88836ed3947c88eb91185459c7bab8b089cc8601
briannalturner <blturner3527@gmail.com>
2020-08-20 12:56:41 -0400

    first commit

---
+++ frontend/src/components/UsersList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class UsersList extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <h1>Users List</h1>
+                <div>
+                    
+                </div>
+            </div>
+        )
+    }
+}
